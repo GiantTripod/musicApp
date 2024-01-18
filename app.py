dbdc290c380078794631d84be9d71e7bff25287c
GiantTripod <chris.mcgrath11@gmail.com>
2024-01-18 12:43:14 -0700

    nav pages - first draft

--- app.py
+++ app.py
@@ -11,6 +11,24 @@ app = Flask(__name__)
 def home():
         return render_template('home.html')
 
+@app.route('/about')
+def about():
+        return render_template('about.html')
+
+
+@app.route('/discovery')
+def discovery():
+        return render_template('discovery.html')
+
+@app.route('/login')
+def login():
+        return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+        return render_template('signup.html')
+
+
 
 
 if __name__ == "__main__":
