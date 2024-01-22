722e4162a9fa3eab93d32d9a1ed7cd8afc6540d3
GiantTripod <chris.mcgrath11@gmail.com>
2024-01-22 15:04:32 -0700

    possible auth solution

--- app/app.py
+++ app/app.py
@@ -1,30 +1,27 @@
-from flask import Flask
-from flask import render_template
-from app.authentication import get_spotify_client
+from flask import Flask, redirect, request, render_template
+from spotipy.oauth2 import SpotifyOAuth
 
 
 app = Flask(__name__)
 
+SPOTIPY_CLIENT_ID = 'e139a0fc9290404996790866f596dd74'
+SPOTIPY_CLIENT_SECRET = 'da931e8a3f19400580c8bf707ba6ce83'
+SPOTIPY_REDIRECT_URI = 'http://localhost:5000/callback'
+
+sp_oauth = SpotifyOAuth(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope='user-library-read')
+
 
 @app.route('/')
 def home():
-        # Get the Spotify client
-        sp = get_spotify_client()
+       auth_url = sp_oauth.get_authorize_url()
+       return redirect(auth_url)
 
-    # Example: Get the user's saved tracks
-        saved_tracks = sp.current_user_saved_tracks(limit=10)
-
-    # Extract relevant information for display
-        tracks_info = [
-        {
-            'name': track['track']['name'],
-            'artists': ', '.join(artist['name'] for artist in track['track']['artists']),
-            'url': track['track']['external_urls']['spotify']
-        }
-        for track in saved_tracks['items']
-        ]
-
-        return render_template('home.html', saved_tracks=tracks_info)
+@app.route('/callback')
+def callback():
+    token_info = sp_oauth.get_access_token(request.args['code'])
+    # Now you can use token_info to make Spotify API requests
+    # For example: sp = spotipy.Spotify(auth=token_info['access_token'])
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
--- app/authentication.py
+++ app/authentication.py
@@ -1,14 +1,14 @@
-import requests, spotipy
+import requests
+import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 def get_spotify_client():
     SPOTIPY_CLIENT_ID = 'e139a0fc9290404996790866f596dd74'
     SPOTIPY_CLIENT_SECRET = 'da931e8a3f19400580c8bf707ba6ce83'
-    SPOTIPY_REDIRECT_URI = 'http://localhost:8888/callback'
+    SPOTIPY_REDIRECT_URI = 'http://localhost:5000/callback'
 
     sp_oauth = SpotifyOAuth(
-        SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET,
-        SPOTIPY_REDIRECT_URI, scope='user-library-read',
+        SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope='user-library-read'
     )
     
     token_info = sp_oauth.get_cached_token()
